a156631d977cca5f09a746a055d0af6acbfa8881
unknown <andrbter@gmail.com>
2025-04-13 18:18:07 -0300

    teste vercel4

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+app.use(express.static('public'));
+
 
 function convertDate(str) {
   if (typeof str !== 'string' || str.trim() === '') return null;
